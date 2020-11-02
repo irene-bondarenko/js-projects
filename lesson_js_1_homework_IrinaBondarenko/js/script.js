44e6db57c6d404669b36421d71f31742b1a0254d
Irina Bondarenko <ren.kol.t@gmail.com>
2020-11-02 14:35:19 +0200

    Change js code

--- lesson_js_1_homework_IrinaBondarenko/js/script.js
+++ lesson_js_1_homework_IrinaBondarenko/js/script.js
@@ -1,9 +1,9 @@
 let name = prompt("Введите Ваше имя");
 let age = prompt("Введите Ваш возраст");
 
-if (16 <= age >0) {
+if (age > 0 & age <=16) {
     console.log("Привет, ", name, "!", "Вы подросток!");
-} else if (40 <= age >= 17) {
+} else if (age >= 17 & age <= 40) {
     console.log("Привет, ", name, "!", "Вы молодой человек");
 } else if (age >= 41) {
     console.log("Привет,", name, "!", "Вы старичок");
