49f8842e7f854f17556ed6207f43ed85419f349b
Irina Bondarenko <ren.kol.t@gmail.com>
2020-11-03 14:32:43 +0200

    Add js code for buttons' click

---
+++ lesson_js_2_homework_IrinaBondarenko/js/script.js
@@ -0,0 +1,19 @@
+let buttonEng = document.querySelector(".button__eng");
+let buttonRus = document.querySelector(".button__rus");
+
+let arr = {
+    "eng": ["Apple", "Orange", "Pineapple", "Cherry"],
+    "rus": ["Яблоко", "Апельсин", "Ананас", "Вишня"]
+}
+
+buttonRus.addEventListener("click", function () {
+for (let i = 0; i < arr["rus"].length; i++){
+        console.log(arr["rus"][i]);
+    }
+})
+
+buttonEng.addEventListener("click", function () {
+for (let i = 0; i < arr["eng"].length; i++){
+        console.log(arr["eng"][i]);
+    }
+})
