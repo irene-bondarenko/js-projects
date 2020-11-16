eeb96ba1abe7b5c0e2a5085443b0e5dd93ba55d1
Irina Bondarenko <ren.kol.t@gmail.com>
2020-11-16 16:52:05 +0200

    Add tabs with color-generator, border-radius and form

---
+++ lesson_js_5_homework_IrinaBondarenko/js/script.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keyup", function (evt) {
+
+        if (evt.keyCode == 32) {
+            let bgColor = document.querySelector(".color-wrap"),
+                hexColorText = document.querySelector(".hex-color");
+            let hex = '#';
+            let range = 'ABCDEF0123456789';
+
+            for (let i = 0; i < 6; i++) {
+                hex += range.charAt(Math.floor(Math.random() * range.length));
+            }
+            bgColor.style.background = hex;
+            hexColorText.innerHTML = hex;
+
+            let e, t, n, r, i = bgColor.style.background;
+            if (i.match(/^rgb/)) {
+                i = i.match(/rgb\(([^)]+)\)/)[1];
+                i = i.split(/ *, */).map(Number);
+                e = i[0];
+                t = i[1];
+                n = i[2]
+            } else if ("#" == i[0] && 7 == i.length) {
+                e = parseInt(i.slice(1, 3), 16);
+                t = parseInt(i.slice(3, 5), 16);
+                n = parseInt(i.slice(5, 7), 16)
+            } else if ("#" == i[0] && 4 == i.length) {
+                e = parseInt(i[1] + i[1], 16);
+                t = parseInt(i[2] + i[2], 16);
+                n = parseInt(i[3] + i[3], 16)
+            }
+
+            r = (e * 299 + t * 587 + n * 114) / 1e3;
+            if (r < 125) {
+                hexColorText.style.color = "white";
+            } else {
+                hexColorText.style.color = "black";
+            }
+        };
+    });
+    let exampleBox = document.querySelector(".example-box");
+    let inputTL = document.querySelector(".inputTL");
+    
+    inputTL.addEventListener("input", function () {
+        let brTL = document.querySelector(".brTL")
+        exampleBox.style.borderTopLeftRadius = this.value + "%";
+        brTL.textContent = "border-top-left-radius: " + this.value + "%"
+    });
+    let inputTR = document.querySelector(".inputTR");
+    inputTR.addEventListener("input", function () {
+        let brTR = document.querySelector(".brTR")
+        exampleBox.style.borderTopRightRadius = this.value + "%";
+        brTR.textContent = "border-top-right-radius: " + this.value + "%"
+    });
+    let inputBR = document.querySelector(".inputBR");
+    inputBR.addEventListener("input", function () {
+        let brBR = document.querySelector(".brBR")
+        exampleBox.style.borderBottomRightRadius = this.value + "%";
+        brBR.textContent = "border-bottom-right-radius: " + this.value + "%"
+    });
+    let inputBL = document.querySelector(".inputBL");
+    inputBL.addEventListener("input", function () {
+        let brBL = document.querySelector(".brBL")
+        exampleBox.style.borderBottomLeftRadius = this.value + "%";
+        brBL.textContent = "border-bottom-left-radius: " + this.value + "%"
+    });
+});
+
+// Get the element with id="defaultOpen" and click on it
+    document.querySelector(".defaultOpen").click();
+    function openTab(evt, tabName) {
+    // Declare all variables
+    let i, tabcontent, tablink;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablink = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
+};
+    
+
+    // Validate and send form
+let phoneFields = document.querySelectorAll(".phoneInput");
+let im = new Inputmask("+38 (099) 99-99-99");
+im.mask(phoneFields);
+
+new JustValidate('.js-form', {
+    rules: {
+        name: {
+            required: true,
+            minLength: 2
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phone: {
+            required: true
+        },
+        message: {
+            required: true
+        }
+    },
+    messages: {
+        name: {
+            required: "Как Вас зовут?",
+            minLength:"Минимальная длина 2 символа"
+        },
+        email: {
+            required: "Как с Вами связаться?",
+            email:"Это не email. Попробуйте еще раз"
+        },
+        phone: {required: "Укажите контактный номер телефона"},
+        message:{required: "Расскажите, о чём хотите узнать"}
+    },
+
+    submitHandler: function (form) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "mail.php", true);
+
+        let formData = new FormData(form);
+        
+        xhr.addEventListener("load", function () {
+            if (xhr.readyState === 4) {
+                switch (xhr.status) {
+                    case 200:
+                        console.log("Form is send");
+                        form.reset();
+                        break;
+                    case 404:
+                        console.log("Oops! Smth went wrong :(");
+                        break;
+                    default:
+                        console.log("Error");
+                        break;
+                }
+            }
+        });
+        xhr.send(formData);
+    },
+  });
+
